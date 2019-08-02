5150553e3269cb0492854311f88918bb800e84b8
enesbcs <lwolf@freemail.hu>
2019-08-02 20:28:33 +0200

    Adding database helper libraries

---
+++ lib/lib_db_mysql.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+#############################################################################
+################ Helper Library for MySQL communication #####################
+#############################################################################
+#
+# Copyright (C) 2019 by Alexander Nagy - https://bitekmindenhol.blog.hu/
+#
+import pymysql
+
+class DB_MySQL():
+
+ def __init__(self):
+  self.connected = False
+  self.db = None
+  self.cur = None
+  self.datasetchanged = False
+  self.writeinprogress = False
+  self.dbname = ""
+
+ def connect(self,hostname=None,dbname=None,username=None,passw=None,dbport=0):
+  self.connected = False
+  if dbname is None or dbname=="":
+   return False
+  if hostname is None or hostname=="":
+   hostname = "localhost"
+  if dbname is None or dbname=="":
+   dbname = "easydata"
+  self.dbname = dbname
+  tc = 0
+  try:
+   self.db = pymysql.connect(host=hostname,user=username,password=passw,db=dbname,charset="utf8mb4")
+   self.cur = self.db.cursor()
+   self.connected = True
+   self.datasetchanged = False
+  except:
+   tc = 1
+  if tc==1:
+   try:
+    if int(dbport)>0:
+     self.db = pymysql.connect(host=hostname,user=username,password=passw,charset="utf8mb4",port=int(dbport))
+    else:
+     self.db = pymysql.connect(host=hostname,user=username,password=passw,charset="utf8mb4")
+    self.cur = self.db.cursor()
+    self.cur.execute("CREATE DATABASE IF NOT EXISTS "+self.dbname+" CHARACTER SET utf8mb4;")
+    self.cur.execute("USE "+self.dbname+";")
+    self.connected = True
+    self.datasetchanged = False
+   except Exception as e:
+    tc = 2
+    print(e)
+    self.connected = False
+
+ def disconnect(self):
+  if self.connected:
+   self.save(True)
+   try:
+    self.db.close()
+   except:
+    pass
+  self.connected = False
+
+ def sqlexec(self,sqlstr=""):
+  succ = False
+  if self.connected==False or sqlstr=="":
+   return False
+  if self.datasetchanged==False:
+   st = sqlstr.strip()
+   if (st[:4].lower()) in ["inse","upda","drop","upda","crea"]:
+    self.datasetchanged = True
+  try:
+   self.cur.execute(sqlstr)
+   succ = True
+  except:
+   succ = False
+
+ def sqlget(self):
+  res = None
+  if self.connected:
+   try:
+    return self.cur.fetchone()
+   except:
+    res = None
+  return res
+
+ def is_writeable(self):
+  return self.connected
+
+ def is_save_needed(self):
+  return self.datasetchanged
+
+ def save(self,WhenNeeded=False):
+  if self.connected:
+   if WhenNeeded:
+    if self.datasetchanged==False: # if not changed pass commit
+     return False
+   try:
+    self.db.commit()
+   except:
+    pass
+  self.datasetchanged = False
+
+ def create_sensortable(self):
+  self.sqlexec("DROP TABLE easysensor")
+  self.sqlexec("CREATE TABLE easysensor (`id` INT NOT NULL AUTO_INCREMENT,`time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,`unit` TINYINT UNSIGNED DEFAULT 0,`nodename` VARCHAR(30) DEFAULT NULL,`tasknum` SMALLINT,`taskname` VARCHAR(30) DEFAULT NULL,`sensortype` SMALLINT DEFAULT 1,`value1` FLOAT,`value2` FLOAT DEFAULT 0,`value3` FLOAT DEFAULT 0,`value4` FLOAT DEFAULT 0,`rssi` SMALLINT default 0,`battery` SMALLINT default 100,`valuetext` VARCHAR(50) DEFAULT NULL, PRIMARY KEY (`id`));")
+  self.save()
+
+ def isexist_sensortable(self):
+  self.sqlexec("SELECT count(*) FROM easysensor")
+  res = self.sqlget()
+  if res is None:
+   res = False
+  else:
+   res = True
+  return res
+
---
+++ lib/lib_db_sqlite.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+#############################################################################
+################ Helper Library for MySQL communication #####################
+#############################################################################
+#
+# Copyright (C) 2019 by Alexander Nagy - https://bitekmindenhol.blog.hu/
+#
+import sqlite3
+import time
+
+class DB_SQLite3():
+
+ def __init__(self):
+  self.connected = False
+  self.db = None
+  self.cur = None
+  self.datasetchanged = False
+  self.writeinprogress = False
+
+ def connect(self,dbname=None):
+  self.connected = False
+  if dbname is None or dbname=="":
+   return False
+  try:
+   self.db = sqlite3.connect(database=dbname,check_same_thread=False)
+   self.cur = self.db.cursor()
+   self.connected = True
+   self.datasetchanged = False
+   self.writeinprogress = False
+  except Exception as e:
+   print(e)
+   self.connected = False
+
+ def disconnect(self):
+  if self.connected:
+   self.save(True) # commit if necessarry
+   try:
+    self.db.close()
+   except:
+    pass
+  self.connected = False
+
+ def sqlexec(self,sqlstr=""):
+  succ = False
+  if self.connected==False or sqlstr=="":
+   return False
+  st = sqlstr.strip()
+  if (st[:4].lower()) in ["inse","upda","drop","upda","crea"]:
+   self.datasetchanged = True
+   if self.writeinprogress:
+     for i in range(0,5):
+      time.sleep(0.1)
+      if self.writeinprogress == False:
+       break
+   self.writeinprogress = True
+  try:
+   self.writeinprogress = False
+   self.cur.execute(sqlstr)
+   succ = True
+  except:
+   succ = False
+
+ def sqlget(self):
+  res = None
+  if self.connected:
+   try:
+    return self.cur.fetchone()
+   except:
+    res = None
+  return res
+
+ def is_writeable(self):
+  res = False
+  if self.connected and self.writeinprogress==False:
+   res = True
+  return res
+
+ def is_save_needed(self):
+  return self.datasetchanged
+
+ def save(self,WhenNeeded=False):
+  if self.connected:
+   if WhenNeeded:
+    if self.datasetchanged==False: # if not changed pass commit
+     return False
+   try:
+    self.db.commit()
+   except:
+    pass
+  self.datasetchanged = False
+
+ def create_sensortable(self):
+  self.sqlexec("DROP TABLE easysensor")
+  self.sqlexec("CREATE TABLE easysensor ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'time' INTEGER DEFAULT CURRENT_TIMESTAMP,'unit' INTEGER DEFAULT 0,'nodename' TEXT DEFAULT NULL,'tasknum' INTEGER,'taskname' TEXT DEFAULT NULL,'sensortype' INTEGER DEFAULT 1,'value1' REAL,'value2' REAL DEFAULT 0,'value3' REAL DEFAULT 0,'value4' REAL DEFAULT 0,'rssi' INTEGER DEFAULT 0,'battery' INTEGER DEFAULT 100,'valuetext' TEXT DEFAULT NULL)")
+  self.save()
+
+ def isexist_sensortable(self):
+  self.sqlexec("SELECT count(*) FROM easysensor")
+  res = self.sqlget()
+  if res is None:
+   res = False
+  else:
+   res = True
+  return res
+
