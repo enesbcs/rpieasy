77887315aad5dbae72ea0461154c6ec367583698
enesbcs <lwolf@freemail.hu>
2020-01-30 19:43:39 +0100

    Adding PZEM-016 support

---
+++ _P206_PZEM016.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+#############################################################################
+####################### PZEM016 plugin for RPIEasy ##########################
+#############################################################################
+#
+# Plugin for the USB-RS485 PZEM-016 device reading
+#
+# Available commands:
+#  PZEMADDRESS,<currentaddress>,<newaddress>   	- change address of pzem device
+#  PZEMRESET,<address>			 	- PZEM reset energy at address
+#
+# Copyright (C) 2020 by Alexander Nagy - https://bitekmindenhol.blog.hu/
+#
+import plugin
+import webserver
+import rpieGlobals
+import rpieTime
+import misc
+import time
+import lib.lib_pzem as uPZEM
+import lib.lib_serial as rpiSerial
+
+class Plugin(plugin.PluginProto):
+ PLUGIN_ID = 206
+ PLUGIN_NAME = "Energy (AC) - PZEM016 USB (TESTING)"
+ PLUGIN_VALUENAME1 = "Volt"
+ PLUGIN_VALUENAME2 = "Amper"
+ PLUGIN_VALUENAME3 = "Watt"
+ PLUGIN_VALUENAME4 = "Wh"
+
+ def __init__(self,taskindex): # general init
+  plugin.PluginProto.__init__(self,taskindex)
+  self.dtype = rpieGlobals.DEVICE_TYPE_SER
+  self.vtype = rpieGlobals.SENSOR_TYPE_QUAD
+  self.valuecount = 4
+  self.senddataoption = True
+  self.recdataoption = False
+  self.timeroption = True
+  self.timeroptional = False
+  self.formulaoption = True
+  self.pzem = None
+  self.readinprogress=0
+  self.initialized=False
+
+ def webform_load(self): # create html page for settings
+  choice1 = self.taskdevicepluginconfig[0]
+  options = rpiSerial.serial_portlist()
+  if len(options)>0:
+   webserver.addHtml("<tr><td>Serial Device:<td>")
+   webserver.addSelector_Head("p206_addr",False)
+   for o in range(len(options)):
+    webserver.addSelector_Item(options[o],options[o],(str(options[o])==str(choice1)),False)
+   webserver.addSelector_Foot()
+   webserver.addFormNote("Address of the USB-RS485 converter")
+  else:
+   webserver.addFormNote("No serial ports found")
+  webserver.addFormNumericBox("Slave address","p206_saddr",self.taskdevicepluginconfig[1],1,247)
+
+  webserver.addFormNote("Default address is 1. Use 'pzemaddress,[currentaddress],[newaddress]' command to change it")
+  if self.taskname=="":
+   choice1 = 0
+   choice2 = 1
+   choice3 = 3
+   choice4 = 5
+  else:
+   choice1 = self.taskdevicepluginconfig[2]
+   choice2 = self.taskdevicepluginconfig[3]
+   choice3 = self.taskdevicepluginconfig[4]
+   choice4 = self.taskdevicepluginconfig[5]
+  options = ["None", "Volt","Amper","Watt", "Wh","Hz","PwrFact"]
+  optionvalues = [-1, 0, 1, 3, 5, 7, 8]
+  webserver.addFormSelector("Indicator1","plugin_206_ind0",len(options),options,optionvalues,None,choice1)
+  webserver.addFormSelector("Indicator2","plugin_206_ind1",len(options),options,optionvalues,None,choice2)
+  webserver.addFormSelector("Indicator3","plugin_206_ind2",len(options),options,optionvalues,None,choice3)
+  webserver.addFormSelector("Indicator4","plugin_206_ind3",len(options),options,optionvalues,None,choice4)
+  return True
+
+ def webform_save(self,params): # process settings post reply
+  par = webserver.arg("p206_saddr",params)
+  try:
+   self.taskdevicepluginconfig[1] = int(par)
+  except:
+   self.taskdevicepluginconfig[1] = 1
+
+  try:
+   self.taskdevicepluginconfig[0] = str(webserver.arg("p206_addr",params)).strip()
+   for v in range(0,4):
+    par = webserver.arg("plugin_206_ind"+str(v),params)
+    if par == "":
+     par = -1
+    else:
+     par=int(par)
+    if str(self.taskdevicepluginconfig[v+2])!=str(par):
+     self.uservar[v] = 0
+    self.taskdevicepluginconfig[v+2] = par
+    if int(par)>0 and self.valuecount!=v+1:
+     self.valuecount = (v+1)
+   if self.valuecount == 1:
+    self.vtype = rpieGlobals.SENSOR_TYPE_SINGLE
+   elif self.valuecount == 2:
+    self.vtype = rpieGlobals.SENSOR_TYPE_DUAL
+   elif self.valuecount == 3:
+    self.vtype = rpieGlobals.SENSOR_TYPE_TRIPLE
+   elif self.valuecount == 4:
+    self.vtype = rpieGlobals.SENSOR_TYPE_QUAD
+  except Exception as e:
+   misc.addLog(rpieGlobals.LOG_LEVEL_ERROR,+str(e))
+  return True
+
+ def plugin_init(self,enableplugin=None):
+  plugin.PluginProto.plugin_init(self,enableplugin)
+  self.taskdevicepluginconfig[0] = str(self.taskdevicepluginconfig[0]).strip()
+  self.readinprogress=0
+  self.initialized=False
+  if self.valuecount == 1:
+    self.vtype = rpieGlobals.SENSOR_TYPE_SINGLE
+  elif self.valuecount == 2:
+    self.vtype = rpieGlobals.SENSOR_TYPE_DUAL
+  elif self.valuecount == 3:
+    self.vtype = rpieGlobals.SENSOR_TYPE_TRIPLE
+  elif self.valuecount == 4:
+    self.vtype = rpieGlobals.SENSOR_TYPE_QUAD
+  if self.enabled and self.taskdevicepluginconfig[0]!="" and self.taskdevicepluginconfig[0]!="0":
+   try:
+    self.pzem = uPZEM.request_pzem_device(self.taskdevicepluginconfig[0],self.taskdevicepluginconfig[1])
+    if self.pzem != None and self.pzem.initialized:
+     misc.addLog(rpieGlobals.LOG_LEVEL_INFO,"PZEM initialized at: "+str(self.taskdevicepluginconfig[0])+" / " +str(self.taskdevicepluginconfig[1]))
+     self.initialized=True
+     self.readinprogress = 0
+    else:
+     misc.addLog(rpieGlobals.LOG_LEVEL_ERROR,"PZEM init failed at address "+str(self.taskdevicepluginconfig[1]))
+   except Exception as e:
+    self.pzem = None
+    misc.addLog(rpieGlobals.LOG_LEVEL_ERROR,"PZEM init error: "+str(e))
+
+ def plugin_read(self): # deal with data processing at specified time interval
+  result = False
+  if self.initialized and self.readinprogress==0 and self.enabled:
+   self.readinprogress = 1
+   for v in range(0,4):
+    vtype = int(self.taskdevicepluginconfig[v+2])
+    if vtype != -1:
+     try:
+      c = 10
+      while self.pzem.busy and c>0:
+       time.sleep(0.05)
+       c=c-1
+      value = self.pzem.read_value(vtype)
+     except:
+      value = None
+     if value != None:
+      self.set_value(v+1,value,False)
+   self.plugin_senddata()
+   self._lastdataservetime = rpieTime.millis()
+   result = True
+   self.readinprogress = 0
+  return result
+
+ def plugin_write(self,cmd):
+  res = False
+  cmdarr = cmd.split(",")
+  cmdarr[0] = cmdarr[0].strip().lower()
+  if cmdarr[0][:4] != "pzem":
+   return False
+  if cmdarr[0] == "pzemaddress":
+      try:
+       ca = int(cmdarr[1].strip())
+       na = int(cmdarr[2].strip())
+      except Exception as e:
+       misc.addLog(rpieGlobals.LOG_LEVEL_ERROR,"Parameter error: "+str(e))
+       return False
+      tpzem = uPZEM.request_pzem_device(self.taskdevicepluginconfig[0],ca)
+      if tpzem:
+       res = tpzem.changeAddress(na)
+      else:
+       res = False
+  elif cmdarr[0] == "pzemreset":
+      try:
+       ca = int(cmdarr[1].strip())
+      except Exception as e:
+       misc.addLog(rpieGlobals.LOG_LEVEL_ERROR,"Parameter error: "+str(e))
+       return False
+      tpzem = uPZEM.request_pzem_device(self.taskdevicepluginconfig[0],ca)
+      if tpzem:
+       res = tpzem.resetenergy()
+      else:
+       res = False
+  return res
---
+++ lib/lib_pzem.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+#############################################################################
+###################### Helper Library for PZEM ##############################
+#############################################################################
+#
+# Copyright (C) 2020 by Alexander Nagy - https://bitekmindenhol.blog.hu/
+#
+import minimalmodbus # sudo pip3 install minimalmodbus
+
+class PZEM():
+
+ VOLT = 0 # voltage - V
+ AMP  = 1 # current - A
+ WATT = 3 # power - W
+ WHR  = 5 # energy - Wh
+ FREQ = 7 # line frequency - Hz
+ PWRF = 8 # power factor
+
+ def __init__(self, port, slaveaddress):
+  self.busy = False
+  self.initialized = False
+  self.port = port
+  self.address = slaveaddress
+  self.connect()
+
+ def connect(self):
+  try:
+   self.dev = minimalmodbus.Instrument(self.port,self.address,close_port_after_each_call=True)
+   self.dev.serial.timeout = 0.1
+   self.dev.serial.baudrate = 9600
+   self.initialized = True
+   self.busy = False
+   testval = self.dev.read_register(self.VOLT,1,4)
+   if testval == None or testval == False:
+    self.initialized = False
+    self.dev = None
+  except Exception as e:
+   self.initialized = False
+   self.dev = None
+
+ def read_value(self, valuetype=0):
+  res = None
+  if self.initialized and self.busy==False:
+   self.busy = True
+   vallen = 2
+   if valuetype in [self.VOLT, self.FREQ]:
+    vallen = 1
+   try:
+    if valuetype in [self.VOLT, self.FREQ, self.PWRF]:
+     res = self.dev.read_register(int(valuetype),int(vallen),4)
+    else:
+     res = self.dev.read_registers(int(valuetype),int(vallen),4)
+     res = res[0]+(res[1]*65536)
+   except Exception as e:
+    pass
+   if res is not None:
+    if valuetype==self.AMP:
+     res = round((res * 0.001),3)
+    elif valuetype==self.WATT:
+     res = round((res * 0.1),1)
+   self.busy = False
+  return res
+
+ def changeAddress(self,newaddress):
+  if newaddress == 0 or newaddress > 247:
+   return False
+  res = False
+  try:
+   self.dev.write_register(2, newaddress, 0, 6)
+   res = True
+  except:
+   res = False
+  if res:
+   self.address = newaddress
+   self.connect()
+  return res
+
+ def resetenergy(self):
+  try:
+   self.dev._performCommand(66,'')
+   res = True
+  except:
+   res = False
+  return res
+
+pzem_devices = []
+
+def request_pzem_device(sport,saddress):
+  for i in range(len(pzem_devices)):
+   if (pzem_devices[i].port == str(sport)) and (int(pzem_devices[i].address) == int(saddress)):
+    return pzem_devices[i]
+  pzem_devices.append(PZEM(sport,saddress))
+  return pzem_devices[-1]
