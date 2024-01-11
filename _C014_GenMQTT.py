1850dec60c5c1d09c0576dfc293f58f74d8f8701
stefan <stefan@clumsy.ch>
2024-01-11 15:52:22 +0100

    Add retain=True to LWT for MQTT
    Add retain=True flag for LWT so that subscribers get LWT status.

--- _C014_GenMQTT.py
+++ _C014_GenMQTT.py
@@ -174,7 +174,7 @@ class Controller(controller.ControllerProto):
    except:
     self.keepalive = 60
    try:
-    self.mqttclient.will_set(self.lwt_t, payload=self.lwtdisconnmsg, qos=0, retain=True)
+    self.mqttclient.will_set(self.lwt_t, self.lwtdisconnmsg, qos=0, retain=True)
     self.mqttclient.connect(self.controllerip,int(self.controllerport),keepalive=self.keepalive) # connect_async() is faster but maybe not the best for user/pass method
     self.mqttclient.loop_start()
    except Exception as e:
@@ -184,7 +184,7 @@ class Controller(controller.ControllerProto):
 
  def disconnect(self):
    try:
-         (mres,mid) = self.mqttclient.publish(self.lwt_t,self.lwtdisconnmsg)
+         (mres,mid) = self.mqttclient.publish(self.lwt_t,self.lwtdisconnmsg, qos=0, retain=True)
    except Exception as e:
          print(e)
    try:
@@ -223,7 +223,7 @@ class Controller(controller.ControllerProto):
      commands.rulesProcessing("GenMQTT#Disconnected",rpieGlobals.RULE_SYSTEM)
     else:
      try:
-         (mres,mid) = self.mqttclient.publish(self.lwt_t,self.lwtconnmsg)
+         (mres,mid) = self.mqttclient.publish(self.lwt_t,self.lwtconnmsg, qos=0, retain=True)
      except:
          pass
      commands.rulesProcessing("GenMQTT#Connected",rpieGlobals.RULE_SYSTEM)
